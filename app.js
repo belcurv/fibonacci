1fc3a0d93475419ab9b813cb33c378fcb6ecb9e8
Jay Schwane <jrschwane@uwalumni.com>
2016-12-06 23:47:56 -0600

    Code cleanup, Better description added to document, add CSS style for 'code'

--- app.js
+++ app.js
@@ -110,25 +110,73 @@
                 */
                 function elementBuilder(iterations) {
                     var squaresArray = generateSquaresArray(iterations),
-                        rectElem;
+                        rectElem,
+                        group,
+                        pathElem,
+                        fillColor,
+                        dAttr;
 
                     // iterate over squaresArray
                     squaresArray.forEach(function (element) {
+                        
+                        fillColor = genFillColor();
+                        
+                        // create <g>roup element
+                        group = angular.element(document.createElementNS(ns, 'g'));
 
                         // create <rect> element & add attributes from squaresArray
                         rectElem = angular.element(document.createElementNS(ns, 'rect'))
+                            .attr('fill', fillColor)
                             .attr('x', element[0])
                             .attr('y', element[1])
                             .attr('width', element[2])
                             .attr('height', element[2]);   // these are squares! ... width = height 
+                        
+                        // create <path> element
+                        pathElem = angular.element(document.createElementNS(ns, 'path'));
+                        
+                        // assemble <path> 'd' attribute
+                        // A rx,ry xAxisRotate LargeArcFlag,SweepFlag x,y"
+                        dAttr = 'M' + element[0] + ',' + (element[1] + element[2]) + ' A' + element[2] + ',' + element[2] + ', 0, 0, 0, ' + (element[0] + element[2]) + ',' + element[1];
+                        
+                        // add attrs to <path>
+                        pathElem
+                            .attr('d', dAttr)
+                            .attr('stroke', '#00ffff')
+                            .attr('stroke-width', 1)
+                            .attr('fill', 'none');
+                        
+                        // append <rect> and <path> to <g>roup
+                        group
+                            .append(rectElem)
+                            .append(pathElem);
 
                         // append <rect> to main <svg> element
-                        svgElem.append(rectElem[0]);
-
+                        svgElem
+                            .append(group);
+                    
                     });
 
                 }
+                
+                
+                /* FILL COLOR RANDOMIZER
+                 *
+                 * @params      [none]
+                 * @returns     [rgba color]
+                */
+                function genFillColor() {
+                    var r = Math.floor(Math.random() * 256),
+                        g = Math.floor(Math.random() * 256),
+                        b = Math.floor(Math.random() * 256),
+                        a = Math.floor(Math.random() * 100) / 100;
+                    
+                    return 'rgba(' + [r, g, b, a].join() + ')';
+                    
+                }
+                
             }
+            
 
             // MR DDO
             return {
