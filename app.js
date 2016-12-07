ce5cc65c898a9c8a8240f490a13e10e12d82c8f1
Jay Schwane <jrschwane@uwalumni.com>
2016-12-06 19:41:12 -0600

    fix angular jqlite grab svg element

--- app.js
+++ app.js
@@ -1,90 +1,61 @@
 (function () {
     'use strict';
-    
+
     angular.module('fibonacci', [])
-    
+
     .controller('fibonacciCtrl', ['$scope', function ($scope) {
-        
-        /*
-        
-        UP: x: -(previous LEFT box's 'x' value), y: -(width + previous LEFT box's 'y' value)
-        LEFT: x: -(width + previous LEFT box's 'x' value) , y: previous UP box's 'y' value
-        DOWN: x: previous LEFT box's 'x' value , y: previous LEFT box's 'y' value + previous
-                 LEFT box's 'height'. NOTE: the 1st element is a DOWN square that breaks the rule.  It's coords are 0,0, dimensions: 1x1.
-        RIGHT: x: previous DOWN box's 'x' value + previous DOWN box's width , y: previous LEFT
-                  box's 'y' value
-                  
-        <rect x="0"     y="0"    width="1"    height="1"   />   <!-- down -->
-        <rect x="0"     y="1"    width="1"    height="1"   />   <!-- right  -->
-        <rect x="0"     y="-2"   width="2"    height="2"   />   <!-- up -->
-        <rect x="-3"    y="-2"   width="3"    height="3"   />   <!-- left -->
-        <rect x="-3"    y="1"    width="5"    height="5"   />   <!-- down -->
-        <rect x="2"     y="-2"   width="8"    height="8"   />   <!-- right -->
-        <rect x="-3"    y="-15"  width="13"   height="13"  />   <!-- up -->
-        <rect x="-24"   y="-15"  width="21"   height="21"  />   <!-- left -->
-        <rect x="-24"   y="6"    width="34"   height="34"  />   <!-- down -->
-        <rect x="10"    y="-15"  width="55"   height="55"  />   <!-- right -->
-        <rect x="-24"   y="-104" width="89"   height="89"  />   <!-- up -->
-        <rect x="-168"  y="-104" width="144"  height="144" />   <!-- left -->
-        <rect x="-168"  y="40"   width="233"  height="233" />   <!-- down -->
-        <rect x="65"    y="-104" width="377"  height="377" />   <!-- right -->
-        <rect x="-168"  y="-714" width="610"  height="610" />   <!-- up -->
-        <rect x="-1155" y="-714" width="987"  height="987" />   <!-- left -->
-        <rect x="-1155" y="273"  width="1597" height="1597" />  <!-- down -->
-        <rect x="442"   y="-714" width="2584" height="2584" />  <!-- right -->
-        
-        */
-        
-        
+
+        var iterations = 19,
+            ns = 'http://www.w3.org/2000/svg',
+            series = generateSeries(iterations);
+
+        $scope.iterations = 19;
+
         // generate squares attribute arrays
         function generateSquaresArray(iterations) {
             // setup
-            var i, c,
-                a = 0,
-                b = a + 1, // init `b` as 1 more than `a`
+            var i,
                 results = [
-                    [0, 0, 1, 1],
-                    [0, 1, 1, 1]
-                ],
-                step = 3,
-                x,
-                y,
-                width,
-                height,
-                series = generateSeries(iterations);
-            
-            console.log(series);
-            
-//            // UP
-//            for (i = 2; i < series.length -1; i += 4) {
-//                var newX,
-//                    newY,
-//                    widthHeight;
-//                
-//                if ( !series[i - 3] ) {
-//                    console.log('element not found');
-//                    newX = 0;
-//                    newY = -2;
-//                    results.push([newX, newY, series[i+1], series[i+1]]);
-//                } else {
-//                    console.log('element found!: ' + series[i - 3]);
-//                    // x: -(previous LEFT box's 'x' value)
-//                    newX = -1 * results[(i - 3)][0];
-//                    // y: -(width + previous LEFT box's 'y' value)
-//                    newY = -1 * (series[i] + results[i - 3][1]);
-//                    results.push([newX, newY, series[i], series[i]]);
-//                }
-//                
-//                console.log(results[i]);
-//                
-//            }
-            
+                [0, -1, 1, 1],
+                [1, 0, 1, 1]
+             ],
+                Xn,
+                Yn;
+
+            for (i = 2; i < series.length - 1; i += 1) {
+
+                // Xn = Math.abs(Yn-1)
+                Xn = Math.abs(results[(i - 1)][1]);
+                // Yn = -((Wn-2) + (Yn-2))
+                Yn = -((results[(i - 2)][2]) - Math.abs((results[(i - 2)][1])));
+                // push new <rect> attrs to results array
+                results.push([Xn, Yn, series[i + 1], series[i + 1]]);
+            }
+
             return results;
+
         }
-        
-        console.log(generateSquaresArray(19));
-        
-        
+
+        // build each <rect> element and attach it to the <svg>
+        (function elementBuilder(iterations) {
+            var squaresArray = generateSquaresArray(iterations),
+                svgElem = angular.element(document.getElementById('fibonacci')),
+                rectElem;
+
+            squaresArray.forEach(function (element) {
+
+                rectElem = angular.element(document.createElementNS(ns, 'rect'))
+                    .attr('x', element[0])
+                    .attr('y', element[1])
+                    .attr('width', element[2])
+                    .attr('height', element[3]);
+
+                svgElem.append(rectElem[0]);
+
+            });
+
+        }(iterations));
+
 
         // generate Fibonacci series
         function generateSeries(iterations) {
@@ -92,8 +63,8 @@
             var i, c,
                 a = 0,
                 b = a + 1, // init `b` as 1 more than `a`
-                results = [0, 1];   // this works
-            
+                results = [0, 1]; // this works
+
             // populate the rest of the array
             for (i = 2; i < iterations; i += 1) {
                 c = a + b; // sum
@@ -102,12 +73,12 @@
                 a = b; // shift a to b's val
                 b = c; // shift b to c's val
             }
-            
+
             return results;
-          }
-        
+        }
+
         $scope.generateSeries = generateSeries;
-        
-    }]);
-    
+
+   }]);
+
 }());
